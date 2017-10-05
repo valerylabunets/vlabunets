30993055a2eddbefa01689ca2fd8f8020b4f10ae
Valery Labunets <vavilonw@gmail.com>
2017-10-05 20:02:30 +0300

    task 4086

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+/**
+ * Calculate.
+ *
+ * @author Valery Labunets (vavilonw@gmail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
