56495800244d122569a56425a02d204fe3fc94ac
Valery Labunets <vavilonw@gmail.com>
2017-11-09 12:10:37 +0200

    Added Profession, Doctor, Engineer, Teacher, ProfessionTest, DoctorTest, TestEngineer, TestTeacher, package-info, package-info

---
+++ chapter_001/src/main/java/ru/job4j/professions/Engineer.java
@@ -0,0 +1,30 @@
+package ru.job4j.professions;
+
+public class Engineer extends Profession {
+    /**
+     * Constructor
+     *
+     */
+    public Engineer(String name, String specialization, int experience) {
+        super(name, specialization, experience);
+    }
+
+    String project;
+
+    /**
+     * Constructor
+     *
+     */
+    public Engineer(String name, String specialization, int experience, String project) {
+        super(name, specialization, experience);
+        this.project = project;
+    }
+    /**
+     * Method project.
+     *
+     * @return String.
+     */
+    String project() {
+        return "Инженер " + super.getName() + " создает " + project;
+    }
+}
