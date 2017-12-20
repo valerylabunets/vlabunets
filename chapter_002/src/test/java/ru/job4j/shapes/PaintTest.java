a9e430a0677867bef0c6b4c36ebe603d14619a8c
Valery Labunets <vavilonw@gmail.com>
2017-12-20 19:12:35 +0200

    Rebooted PaintTest

--- chapter_002/src/test/java/ru/job4j/shapes/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/shapes/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.shapes;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -16,35 +18,40 @@ import static org.junit.Assert.*;
  * @since 0.1
  */
 public class PaintTest {
+
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     /**
      * Test  draw.
      */
     @Test
     public void whenDrawSquare() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфер для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
         new Paint().draw(new Square());
-        // проверяем результат вычисления
         assertThat(new String(out.toByteArray()), is(new StringBuilder()
                 .append("++++")
                 .append("+  +")
                 .append("+  +")
                 .append("++++")
                 .append(System.lineSeparator()).toString()));
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(new String(out.toByteArray()), is(new StringBuilder()
                   .append("   ^   ")
@@ -52,6 +59,5 @@ public class PaintTest {
                   .append(" ^   ^ ")
                   .append("^^^^^^^")
                   .append(System.lineSeparator()).toString()));
-        System.setOut(stdout);
     }
 }
